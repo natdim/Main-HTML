04df9fc86d9991e684f58f173699367d534c1d8b
gsevtlana <gsevtlana@gmail.com>
2018-07-20 21:38:48 -0700

    adding new button for minus (#7)
    * Update index.html
    
    * adding button 'minus'
    
    * adding sty
    
    * Update script.js
    
    * adding subtract method
    
    * update for button minus - added function onclick

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function toInput(id) {
     let value = document.getElementById(id).value;
     document.getElementById('result').value += value;
 }
+
+function subtract(){
+    const a = document.getElementById('').value;
+    const b = document.getElementById('').value;
+    return a-b;
+}
